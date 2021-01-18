6dc83b9d5e3c75303fa2837bc60c97a4f429f9a5
Mike Leavy <mleavy@gmail.com>
2021-01-18 11:45:24 -0800

    upstream merge, plus SPM support

--- Package.swift
+++ Package.swift
@@ -1,15 +1,20 @@
-// swift-tools-version:5.1
+    // swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "SVProgressHUD",
-    platforms: [
-        .iOS(.v10)
-    ],
-    products: [
-        .library(name: "SVProgressHUD", targets: ["SVProgressHUD"])
-    ],
-    targets: [
-        .target(name: "SVProgressHUD", path: "SVProgressHUD")
-    ]
+  name: "SVProgressHUD",
+  platforms: [
+    .iOS(.v8), .tvOS(.v9)
+  ],
+  products: [
+    .library(name: "SVProgressHUD", targets: ["SVProgressHUD"])
+  ],
+  targets: [
+    .target(
+      name: "SVProgressHUD",
+      path: "SVProgressHUD",
+      exclude: ["SVProgressHUD-Prefix.pch"],
+      resources: [.copy("SVProgressHUD.bundle")],
+      publicHeadersPath: ".")
+  ]
 )
